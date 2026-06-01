10f2b10f7192b71d609d7bccf154bce93447ac88
Lucas Santos <79930094+lucsanto@users.noreply.github.com>
2026-06-01 17:30:42 -0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(char *str);
+char	*free_null(char *ptr);
+char	*read_to_total(int fd, char *total);
+char	*get_next_line(int fd);
+
+int	    newline_not_found(char *str);
+char	*init_empty(void);
+char	*join_to_total(char *total, char *buffer);
+char	*extract_line(char *total);
+char	*save_rest(char *total);
+
+#endif
