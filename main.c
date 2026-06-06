236c5913bb21dc8fd0c635ebc167f56da0036fc4
Lucas Santos <79930094+lucsanto@users.noreply.github.com>
2026-06-06 11:03:48 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//mandatory
+#include "get_next_line.h"
+
+//bonus
+//#include "get_next_line_bonus.h"
+
+void	print_gnl(int fd)
+{
+	char	*line;
+
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+}
+
+int	main(void)
+{
+	int	fd1;
+	// int	fd2;
+
+	printf("-------mandatory-------\n");
+	fd1 = open("a.txt", O_RDONLY);
+	if (fd1 < 0)
+		return (1);
+	print_gnl(fd1);
+	close(fd1);
+
+	printf("\n------fd inválido-------\n");
+	if (get_next_line(-1) == NULL)
+		printf("fd invalido retornou NULL\n");
+
+	// printf("\n-------bonus-------\n");
+	// fd1 = open("a.txt", O_RDONLY);
+	// fd2 = open("b.txt", O_RDONLY);
+	// if (fd1 < 0 || fd2 < 0)
+	// 	return (1);
+
+	// printf("fd1: %s", get_next_line(fd1));
+	// printf("fd2: %s", get_next_line(fd2));
+	// printf("fd1: %s", get_next_line(fd1));
+	// printf("fd2: %s", get_next_line(fd2));
+	// printf("fd1: %s", get_next_line(fd1));
+	// printf("fd2: %s", get_next_line(fd2));
+
+	close(fd1);
+	// close(fd2);
+	printf("\n");
+	return (0);
+}
